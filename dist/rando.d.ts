4a46439dfbb28d3a38f3287b62bc841c8418989f
Tim Caswell <creationix@vercel.com>
2024-10-17 20:03:07 -0500

    Lots of cleanups

---
+++ dist/rando.d.ts
@@ -0,0 +1,18 @@
+export declare function decodeB64(buf: Uint8Array, offset?: number, end?: number): [number | bigint, number];
+export declare function encodeB64(num: bigint | number): number[];
+export declare function splitDecimal(val: number): (number | bigint)[];
+export interface EncodeOptions {
+    chainMinChars?: number;
+    chainSplitter?: RegExp;
+    prettyPrint?: boolean;
+    knownValues?: unknown[];
+    binaryHeaders?: boolean;
+    streamContainers?: boolean;
+}
+export declare function findStringSegments(rootVal: unknown, options?: EncodeOptions): {
+    [val: string]: number;
+};
+export declare function continuedFractionApproximation(num: number, maxIterations?: number, tolerance?: number): number[];
+export declare function encodeBinary(rootVal: unknown, options?: EncodeOptions): Uint8Array;
+export declare function stringify(rootVal: unknown, options?: EncodeOptions): string;
+export declare function encode(rootVal: unknown, options?: EncodeOptions): Uint8Array;
